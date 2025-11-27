7daaebf3c01181f20bacca06bbee5fd7f3c82558
Safe <drissi.safe2003@gmail.com>
2025-11-27 21:46:58 +0100

    resolving conflict

--- main.c
+++ main.c
@@ -1,37 +1,24 @@
+#include "./Config/types.h"
+#include "./Config/config.h"
+#include "./Algorithms/Algorithms.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
-
-char* PATH = "./Config/config.txt";
-
-int main(void){
+	@@ -12,7 +8,7 @@ int main(void){
     Config* CFG = malloc(sizeof(Config));
     int config_load_res;
     config_load_res = load_config(PATH , CFG);
-
+    
     if(config_load_res == 0) {
         printf("Error loading config file.\n");
     }else if (config_load_res == 1) {
-        for(int i=0; i<CFG -> process_count; i++){
-            PROCESS p = CFG -> processes[i];
-            printf("Process ID: %s\n", p.ID);
-            printf("  Coming Time: %d\n", p.arrival_time);
-            printf("  Execution Time: %d\n", p.execution_time);
-            printf("  Priority: %d\n", p.priority);
-            printf("  %s has %d IO Operations;\n",p.ID, p.io_count);
-            for(int j=0; j<p.io_count; j++){
-                IO_OPERATION io = p.io_operations[j];
-                printf("    IO Operation %d: Start Time = %d, Duration = %d\n", j+1, io.start_time, io.duration);
-            }
-        }
-        printf("Configuration loaded successfully.\n");
-        printf("Let s test Round Robin Scheduling Algorithm...\n");
-        RoundRobin_Algo(CFG);
+	@@ -34,8 +30,8 @@ int main(void){
         printf("Testing Multilevel Scheduler with Aging...\n");
         MultilevelAgingScheduler(CFG);
 
-
+        
     }
-
+    
    return 0;
 }
