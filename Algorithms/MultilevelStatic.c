4b70cf898d68d733108133382db206a27e17dfba
Lourimi Wassim <wassimlourimi@gmail.com>
2025-12-07 15:11:53 +0100

    Update MultilevelStatic.c

--- Algorithms/MultilevelStatic.c
+++ Algorithms/MultilevelStatic.c
@@ -5,45 +5,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define QUANTUM 2
-
-
 void MultilevelStaticScheduler(Config* config) {
-    PCB* pcbs = initialize_PCB(config); // do NOT free
+
+    int QUANTUM;
+    printf("Enter the quantum value: ");
+    scanf("%d", &QUANTUM);
+
+    if (QUANTUM <= 0) {
+        printf("Invalid quantum. Using default quantum = 2\n");
+        QUANTUM = 2;
+    }
+
+    PCB* pcbs = initialize_PCB(config);
     int time = 0;
     int finished_processes = 0;
     int total_processes = config->process_count;
+
+    printf("\n=== Multilevel Static Scheduler Start ===\n");
+
     
-    printf("=== Multilevel Static Scheduler Start ===\n");
-    
-    // Main loop
     while (finished_processes < total_processes) {
+
         PCB* next = NULL;
-        
-        // Pick highest priority ready process
+
+      
         for (int i = 0; i < total_processes; i++) {
-            if (!pcbs[i].finished && !pcbs[i].in_io && pcbs[i].process.arrival_time <= time) {
+            if (!pcbs[i].finished &&
+                !pcbs[i].in_io &&
+                pcbs[i].process.arrival_time <= time) {
+
                 if (!next || pcbs[i].process.priority > next->process.priority) {
                     next = &pcbs[i];
                 }
             }
         }
-        
+
         if (next) {
-            int run_time = 2; // RR quantum
-            if (next->remaining_time < run_time) run_time = next->remaining_time;
-            printf("Time %d-%d: |%-4s", time, time+run_time, next->process.ID);
+            int run_time = QUANTUM;
+            if (next->remaining_time < run_time)
+                run_time = next->remaining_time;
+
+            printf("Time %d-%d: |%-4s (Priority %d)\n",
+                   time,
+                   time + run_time,
+                   next->process.ID,
+                   next->process.priority);
+
             next->remaining_time -= run_time;
             time += run_time;
+
             if (next->remaining_time <= 0) {
                 next->finished = 1;
                 finished_processes++;
+                printf("Process %s finished at time %d\n",
+                       next->process.ID, time);
             }
         } else {
             printf("Time %d: CPU idle\n", time);
             time++;
         }
     }
-    
-    printf("\n=== Multilevel Static Scheduler End ===\n");
+
+    printf("=== Multilevel Static Scheduler End ===\n");
 }
