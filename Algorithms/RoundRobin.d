ee87c07793bb298418a72b4ee952e3bf51edb3fc
Lourimi Wassim <wassimlourimi@gmail.com>
2025-12-10 22:45:38 +0100

    Updated gitignore
    Removed any unnecessary files after compilation and building
    cleaner repo

--- Algorithms/RoundRobin.d
+++
@@ -1,7 +0,0 @@
-Algorithms/RoundRobin.o: Algorithms/RoundRobin.c \
- Algorithms/../Config/types.h Algorithms/../Config/config.h \
- Algorithms/../Config/types.h Algorithms/Algorithms.h
-Algorithms/../Config/types.h:
-Algorithms/../Config/config.h:
-Algorithms/../Config/types.h:
-Algorithms/Algorithms.h:
--- main.d
+++
@@ -1,9 +0,0 @@
-main.o: main.c Config/types.h Config/config.h Config/types.h \
- Algorithms/Algorithms.h Algorithms/../Config/config.h \
- Algorithms/../Config/types.h
-Config/types.h:
-Config/config.h:
-Config/types.h:
-Algorithms/Algorithms.h:
-Algorithms/../Config/config.h:
-Algorithms/../Config/types.h:
